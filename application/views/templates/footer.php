e7ff0bcfc836e9d94c8e6ac45ad8c663480fabdf
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2020-09-18 14:40:49 +0700

    perbaikan bug

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -7,7 +7,6 @@
  <script src="<?= base_url(); ?>assets/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
  <script src="<?= base_url(); ?>assets/js/jquery.easing.1.3.js"></script>
  <script src="<?= base_url(); ?>assets/js/script.js"></script>
- <script src="<?= base_url(); ?>assets/js/sb-admin-datatables.min.js"></script>
  <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
  <script>
    $(function() {
