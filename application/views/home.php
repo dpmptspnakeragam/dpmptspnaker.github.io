d3327820640a395aa2bf538d2699f7c9d8c2a4d8
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2020-12-23 16:00:14 +0700

    perbaikan bug

--- application/views/home.php
+++ application/views/home.php
@@ -181,7 +181,7 @@
         <a href="https://app.oss.go.id/app/#front/home" class="pilih-pelayanan"><img id="img-layanan" class="mb-2 img-layanan" src="<?= base_url(); ?>assets/img/oss.jpg" alt="gambaross" width="100%"> OSS</a>
       </div>
       <div class="col col-sm-12 col-md-4 col-lg-4 col-6 display-4 mb-3">
-        <a href="https://account.kemnaker.go.id/auth/login" class="pilih-pelayanan"><img id="img-layanan" class="mb-2 img-layanan" src="<?= base_url(); ?>assets/img/sisnaker.jpg" alt="gambarsicantikcloud" width="100%"> Tracking SiCantik</a>
+        <a href="https://account.kemnaker.go.id/auth/login" class="pilih-pelayanan"><img id="img-layanan" class="mb-2 img-layanan" src="<?= base_url(); ?>assets/img/sisnaker.jpg" alt="gambarsicantikcloud" width="100%"> SISNAKER KEMNAKER</a>
       </div>
     </div>
     <div class="row text-center">
@@ -189,7 +189,7 @@
         <a href="https://sicantikui.layanan.go.id" class="pilih-pelayanan"><img id="img-layanan" class="mb-2 img-layanan" src="<?= base_url(); ?>assets/img/sicantikcloud.jpg" alt="gambarsicantikcloud" width="100%"> SiCantik</a>
       </div>
       <div class="col col-sm-12 col-md-4 col-lg-6 col-6 display-4 mb-3">
-        <a href="" class="pilih-pelayanan" data-toggle="modal" data-target="#ModalTracking"><img id="img-layanan" class="mb-2 img-layanan" src="<?= base_url(); ?>assets/img/trackingsicantik.jpg" alt="gambarsicantikcloud" width="100%"> SISNAKER KEMNAKER</a>
+        <a href="" class="pilih-pelayanan" data-toggle="modal" data-target="#ModalTracking"><img id="img-layanan" class="mb-2 img-layanan" src="<?= base_url(); ?>assets/img/trackingsicantik.jpg" alt="gambarsicantikcloud" width="100%"> Tracking SiCantik</a>
       </div>
     </div>
   </div>
