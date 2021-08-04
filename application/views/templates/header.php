43039e4a41398a88c37fa977eb1c56905e73082b
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2021-08-04 14:54:01 +0700

    Perbaikan header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,24 +7,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css">
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
-  <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/datatables.min.css" />
-  <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/vectoragam.png">
-  <script src="<?php echo base_url() ?>/assets/js/Chart.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.js"></script>
+  <link defer rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css">
+  <link defer rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link defer rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link defer href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap" rel="stylesheet">
+  <link defer rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <link defer rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+  <link defer rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+  <link defer rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/datatables.min.css" />
+  <link defer rel="shortcut icon" href="<?= base_url(); ?>assets/img/vectoragam.png">
+  <script defer src="<?php echo base_url() ?>/assets/js/Chart.js"></script>
+  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.js"></script>
 
-  <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-  <script src="<?= base_url(); ?>assets/js/script.js"></script>
+  <script defer src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script defer src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script defer src="<?= base_url(); ?>assets/js/script.js"></script>
 
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@300&display=swap" rel="stylesheet">
+  <link defer rel="preconnect" href="https://fonts.gstatic.com">
+  <link defer href="https://fonts.googleapis.com/css2?family=Spectral:wght@300&display=swap" rel="stylesheet">
 
   <title>DPMPTSP-Naker Kab. Agam</title>
 </head>
