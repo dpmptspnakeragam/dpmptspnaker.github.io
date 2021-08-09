95b37aef2d551b9c3742a70db625e5c376d258d9
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2021-08-09 12:26:31 +0700

    defer image

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -19,6 +19,18 @@
      $('#dataTable').dataTable({});
    });
  </script>
+ <script>
+   function init() {
+     var imgDefer = document.getElementsByTagName('img');
+     for (var i = 0; i < imgDefer.length; i++) {
+       if (imgDefer[i].getAttribute('data-src')) {
+         imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+       }
+     }
+   }
+
+   window.onload = init;
+ </script>
  </body>
 
  </html>
