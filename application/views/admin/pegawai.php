7808f97247d040ca8eac40ef381726bc753960db
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2024-06-24 10:11:40 +0700

    menghapus tabel pejabat DPMPTSP Agam pada Menu Profil/Pegawai di Admin

--- application/views/admin/pegawai.php
+++ application/views/admin/pegawai.php
@@ -75,49 +75,6 @@
                     </table>
                 </div>
                 <!--end: Accordion -->
-
-                <h3 class="text-center">Pejabat DPMPTSP Kabupaten Agam</h3>
-                <hr>
-                <!-- start: Accordion -->
-                <div class="table-responsive">
-                    <table class="table table-striped table-borderless table-hover" id="dataTables-example">
-                        <thead class="bg-dark text-light">
-                            <tr>
-                                <th class="text-center">No.</th>
-                                <th class="text-center">Nama</th>
-                                <th class="text-center">NIP</th>
-                                <th class="text-center">Jabatan</th>
-                                <th class="text-center">Golongan</th>
-                                <th class="text-center">Alamat</th>
-                                <th class="text-center">Gambar</th>
-                                <th class="text-center"><i class="fa fa-cog"></i> Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $no = 1;
-                            foreach ($kabid->result() as $row) {
-                            ?>
-                                <tr class="odd gradeX">
-                                    <td><?= $no++; ?></td>
-                                    <td><?= $row->nama_kabid; ?></td>
-                                    <td><?= $row->nip_kabid; ?></td>
-                                    <td><?= $row->jabatan_kabid; ?></td>
-                                    <td><?= $row->golongan_kabid; ?></td>
-                                    <td><?= $row->alamat_kabid; ?></td>
-                                    <td><img src="<?= base_url(); ?>assets/imgupload/<?= $row->gambar_kabid; ?>" style="width:95%;" class="img-responsive"></td>
-                                    <td class="text-center">
-                                        <div class="btn-group">
-                                            <a class="btn btn-outline-warning btn-sm btn-circle" href="#" data-toggle="modal" data-target="#EditKabid<?php echo $row->id_kabid; ?>" title="Edit"><i class="fa fa-edit"></i></a>
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
-                <!--end: Accordion -->
-
             </div>
         </div>
     </div>
