856d4ae0451786fc38bb5f83dbbf31df2fb86016
dpmptspnakeragam <dpmptspnakeragam@gmail.com>
2024-08-12 11:47:59 +0700

    5

--- application/config/email.php
+++ application/config/email.php
@@ -4,14 +4,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $config = array(
     'protocol'          => 'smtp',
     'smtp_host'         => 'mail.dpmptsp.agamkab.go.id',
-    'smtp_port'         => 587,  // Port for TLS
+    'smtp_port'         => 587,
     'smtp_user'         => 'pengaduan@dpmptsp.agamkab.go.id',
     'smtp_pass'         => 'p_ptsp@99agam',
-    'smtp_crypto'       => 'tls', // Use TLS
+    'smtp_crypto'       => 'tls',
     'mailtype'          => 'html',
     'charset'           => 'UTF-8',
-    'wordwrap'          => TRUE,
     'newline'           => "\r\n",
-    'smtp_timeout'      => 10,
-    'validate'          => TRUE
 );
